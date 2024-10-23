2c4d2f2d903375869d4b2103a34e47b88895bb0d
James <james@kmotiondesign.com>
2024-10-23 11:35:26 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# James Sudimak - Interactive 2D Portfolio
+
+![2D Portfolio Map](./public/map.png)
+
+This is an interactive "game" portfolio built using [Kaboom JS](https://kaboomjs.com/) and [Tiled](https://www.mapeditor.org/).
+
+It is based on [this project](https://github.com/JSLegendDev/2d-portfolio-kaboom) created by JSLegendDev.
---
+++ src/constants.js
@@ -0,0 +1,50 @@
+export const scaleFactor = 4;
+
+export const dialogueDataExample = {
+    pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
+    I've made a couple of games in that language. I also like Golang and Python. Anyway regardless of the language, I just like programming.
+    Here is my <a href="https://github.com/jslegenddev" target="_blank">Github</a>!`,
+    "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
+    "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
+    I also make game programming tutorials on YouTube. Go sub to <a href="https://youtube.com/@jslegenddev" target="_blank">my channel</a>! (If you like the content)
+    You'll learn how I built this portfolio you're currently playing through!`,
+    tv: `That's my TV. I've been watching tech youtubers a lot recently like :
+     <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
+    <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
+    bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+    resume: `This is my desk and on it is my resume. <a href="https://github.com/JSLegendDev/Resume/blob/main/JSLegend%20Resume-1.pdf" target="_blank">Check it out?</a>
+    Contact me at jslegend@protonmail.com if you have any interesting job opportunities!`,
+    projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+    Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
+    Kaboom.js by watching some of my tutorials <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
+    library: `There are a lot of programming books on my shelves. There is even one in French (I also speak French btw).
+    I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
+    exit: `If you want to exit JSLegendDev's portfolio, just close the tab.`,
+};
+
+export const dialogueData = {
+  pc: `This is my personal PC. This is where I spend quality leisure time on the Internet, game with my friends, as well as record, edit, and upload content for my YouTube channel <a href="https://www.youtube.com/channel/UCdp4eZYZ1IvMhsSGAtE9grQ" target="blank">Oscar Guy Here</a>!`,
+  webdev: `This is my Mac Mini workstation. This is where I code and make beautiful websites!
+  To see some of the work I've created and agencies I've created for, please check out my <a href="https://jsudimak.com/webdev" target="__blank">website</a> and my <a href="https://github.com/JazzMusic131" target="__blank">GitHub</a>.`,
+  resume: `This is my resume; I've been updating it as of late!
+  Learn more about what I've done at my <a href="https://www.linkedin.com/in/james-sudimak-62091410a" target="__blank">LinkedIn profile</a> or on my <a href="https://jsudimak.com/webdev" target="__blank">website</a>!`,
+  degree: `This is my Associate of Science Degree from Delaware Technical Community College. It's for Computer Information Systems, but I ended up being a web developer!
+  My time at Del Tech did not go to waste however; it provided me with a great foundation.`,
+  endtable: `This is my alarm clock. You best believe it's on full blast at 7:30AM!
+  It <em>usually</em> works.`,
+  bed: `It's just my bed; don't get too excited.`,
+  gaming: `This is my gaming center! This is where I enjoy playing my game consoles whenever I have the downtime, which can be seldom these days.
+  Right now, the SNES and GameCube are hooked up, two consoles I grew up playing fondly.`,
+  jazzmage: `Meet the Prince of Darkness! He represents my musician side since this spritesheet doesn't contain anything music-related (frown).
+  I went to music school for jazz drums at The New School for Jazz for a couple years. Those two years were essential for me as a musician and as a maturing adult. I still play and compose music whenever the inspiration strikes!`,
+  books: `This is my bookshelf! I like to read in theory, but I'm more of an audiobook kind of guy. Lately, I've been checking out memiors of musicians: Serj Tankian, Dave Grohl, Phil Collins.
+  My favorite book? The Autobiography of Miles Davis. Wild!!`,
+  window: `What a beautiful day in Philadelphia! It isn't my stomping ground, but it's a place I've called home since 2019.`,
+  couch: `This is where I relax and watch movies. I'm a film buff who runs a side hustle called <a href="https://oscarguyhere.com" target="__blank">Oscar Guy Here</a>, so I spend a lot of my time between here and the movie theater to catch up on the latest developments in film!`,
+  table: `Just a coffee table. I put drinks here, sometimes food, sometimes my feet ¯\\_(ツ)_/¯`,
+  movies: `What's in here now? ... Ah! Punch Drunk Love (2002) directed by Paul Thomas Anderson. Classic! Still Adam Sandler's best performance (sorry Uncut Gems stans)!`,
+  fridge: `This is my fridge where I keep perishable consumables like a standard human. Right not I'm chilling some cold brew in here.
+  (Why does it have to take 24 hours?!)`,
+  exit: `If you want to exit James Sudimak's 2D portfolio, just close the tab.
+  Thanks for stopping in!!`
+};
